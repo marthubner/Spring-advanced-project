c3bcf9ea86bcfe9fe5c5b64bfd43e78b63a2237c
Ben <Benedikt.Vitek@gmail.com>
2023-12-05 10:31:12 +0100

    updated dependency for retrofit, updated security to permitAll

---
+++ src/main/java/com/greenfox/advancedspringproject/config/ProjectSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.greenfox.advancedspringproject.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authRequest -> authRequest
+                        .anyRequest().permitAll()))
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(httpBasic -> httpBasic.disable());
+        return http.build();
+
+    }
+
+
+}
---
+++ src/main/java/com/greenfox/advancedspringproject/controller/MovieController.java
@@ -0,0 +1,16 @@
+package com.greenfox.advancedspringproject.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MovieController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "test";
+    }
+}
