021cd7134274a6c49142503eba6ffb78c34f0078
Martin Hübner <marthubner@gmail.com>
2023-12-05 09:42:47 +0100

    start the project

---
+++ src/main/java/com/greenfox/advancedspringproject/AdvancedSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.advancedspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedSpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greenfox/advancedspringproject/AdvancedSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfox.advancedspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
